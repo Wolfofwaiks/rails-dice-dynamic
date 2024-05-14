8a258a147325337817c357358d986519f2dce2f9
Wolfofwaiks <165302970+Wolfofwaiks@users.noreply.github.com>
2024-05-14 01:49:52 +0000

    completed with 100%

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -5,40 +5,17 @@ class ZebraController < ApplicationController
   end 
 
   def giraffe 
-    @first_die = rand(1..6)
-    @second_die = rand(1..6)
-    render({ :template=> "templates/twodsix" })
-  end 
-
-  def elephant
-    @first_die = rand(1..10)
-    @second_die = rand(1..10)
-    render({ :template=> "templates/twodten" })
-  end
-
-  def lion 
-    @die = rand(1..20)
-    render({ :template=> "templates/onedtwenty" })
-  end 
-
-  def hyena
-    @first_die = rand(1..4)
-    @second_die = rand(1..4)
-    @third_die = rand(1..4)
-    @fourth_die = rand(1..4)
-    @fifth_die = rand(1..4)
-    render({ :template=> "templates/fivedfour" })
-  end 
-
-  def hippo
+    @num_dice = params.fetch("numdice").to_i
+    @sides = params.fetch("numsides").to_i
+  
     @rolls = []
-
-    100.times do
-      die = rand(1..6)
+  
+    @num_dice.times do
+      die = rand(1..@sides)
   
       @rolls.push(die)
-      
-    render({ :template=> "templates/onehundreddsix" })
-  end
+    end
+    render({ :template=> "templates/flexible" })
+  end 
 
 end 
